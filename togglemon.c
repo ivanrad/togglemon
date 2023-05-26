b16e340161b0733d3d7e661ed3fc417f5d6dc044
ivanrad <ivanra@gmail.com>
2023-05-26 11:22:49 +0100

    add dry run option

--- togglemon.c
+++ togglemon.c
@@ -1,5 +1,9 @@
 // SPDX-License-Identifier: MIT
 //
+// toggle MacBook's built-in display on/off
+// usage: togglemon [-n]
+//   -n    dry run
+//
 #include <stdlib.h>
 #include <CoreGraphics/CoreGraphics.h>
 
@@ -18,12 +22,14 @@ static void die(char *msg) {
   exit(EXIT_FAILURE);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
   const uint32_t max_display = 0x20;
   CGDirectDisplayID display_ids[max_display];
   CGDirectDisplayID main_display_id;
   uint32_t display_count;
   CGError err;
+
+  int dry_run = argc == 2 && argv[1][0] == '-' && argv[1][1] == 'n';
 
   main_display_id = CGMainDisplayID();
 
@@ -45,21 +51,24 @@ int main(void) {
     int height = CGDisplayPixelsHigh(display_id);
 
     fprintf(stdout,
-      "display_id: %u, res: %d" "x" "%d, is_builtin: %d, "
-      "is_online: %d, is_active: %d, is_main: %d\n",
+      "display_id: %u, res: %d" "x" "%d, builtin: %d, "
+      "online: %d, active: %d, main: %d\n",
       display_id, width, height, is_builtin, is_online, is_active, is_main);
 
     if (is_builtin) {
       CGDisplayConfigRef config;
 
-      fprintf(stdout, "toggling display_id %u...\n", display_id);
+      fprintf(stdout, "%stoggling display_id %u...\n",
+          dry_run ? "[DRY RUN] " : "", display_id);
 
-      if ((err = CGBeginDisplayConfiguration(&config)) != kCGErrorSuccess)
-        die("CGBeginDisplayConfiguration failed");
-      if ((err = CGSConfigureDisplayEnabled(config, display_id, !is_active)) != kCGErrorSuccess)
-        die("CGSConfigureDisplayEnabled failed");
-      if ((err = CGCompleteDisplayConfiguration(config, kCGConfigurePermanently)) != kCGErrorSuccess)
-        die("CGCompleteDisplayConfiguration failed");
+      if (!dry_run) {
+        if ((err = CGBeginDisplayConfiguration(&config)) != kCGErrorSuccess)
+          die("CGBeginDisplayConfiguration failed");
+        if ((err = CGSConfigureDisplayEnabled(config, display_id, !is_active)) != kCGErrorSuccess)
+          die("CGSConfigureDisplayEnabled failed");
+        if ((err = CGCompleteDisplayConfiguration(config, kCGConfigurePermanently)) != kCGErrorSuccess)
+          die("CGCompleteDisplayConfiguration failed");
+      }
     }
   }
   exit(EXIT_SUCCESS);
